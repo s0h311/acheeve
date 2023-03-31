7096b346edd515f0c562a9f10d84dd9568caa2f6
Soheil Nazari <kontakt@soheilnazari.de>
2023-03-31 14:42:27 +0200

    favicon, head title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Acheeve | Habits & more',
+      link: [{rel: 'icon', type:'image/x-icon', href:'/favicon.ico'}]
+    }
+  },
   modules: [
     '@vueuse/nuxt',
     '@nuxtjs/supabase',
