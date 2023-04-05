e834c368a5f47e9a8390e8841566b1d66a3e56a5
René Scheuer <rene.scheuer03@gmail.com>
2023-04-05 15:36:05 +0200

    AC-11

--- pages/setNewPassword.vue
+++ pages/setNewPassword.vue
@@ -1,11 +1,43 @@
 <template>
-  <div>
-    <InputField/>
-    <InputField/>
-    <InputButton></InputButton>
+  <div class="grid place-items-center gap-5 pt-56">
+    <h1 class="text-primary text-5xl font-semibold mb-10">{{ $t('password_reset') }}</h1>
+
+    <InputField
+    name="password"
+    label="New Password"
+    placeholder="*******"
+    @change-input="onchange"/>
+
+    <InputField
+    name="passwordConfirm"
+    label="Confirm Password"
+    placeholder="*******"
+    @change-input="onchange"/>
+
+    <InputButton :text="$t('set_new_password')" @click="handle" />
   </div>
 </template>
-
+  
 <script setup>
 
+const passwords = useState('passwords', () => {
+  return {
+    password: '', 
+    passwordConfirm: ''
+  }
+})
+const supabase = useSupabaseClient();
+
+const handle = async () => {
+  if (passwords.value.password === passwords.value.passwordConfirm) {
+    const { data, error } = await supabase.auth.updateUser({
+      password: passwords.value.password
+    })
+  }
+  navigateTo('/')
+} 
+
+const onchange = (name, input) => {
+  passwords.value[name] = input
+}
 </script>
