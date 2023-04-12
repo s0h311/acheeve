58f499285c659b00d2d417459e6c7f340db3ecb4
Soheil Nazari <kontakt@soheilnazari.de>
2023-04-12 18:37:18 +0200

    Eslint and Prettier

--- composables/useUserProfile.ts
+++ composables/useUserProfile.ts
@@ -1,10 +1,7 @@
 export default async (id: string) => {
   const supabase = useSupabaseClient()
 
-  const { data: profile, error } = await supabase   
-    .from('profiles')
-    .select()
-    .eq('id', id)
+  const { data: profile, error } = await supabase.from('profiles').select().eq('id', id)
 
-    return profile
+  return { profile, error }
 }
