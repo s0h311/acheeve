c4412a7f6ed26634e3b7e451629b85d0110a6313
Soheil Nazari <kontakt@soheilnazari.de>
2023-04-25 22:09:17 +0200

    AC-34 Error Page

---
+++ error.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="statusCode">{{ error.statusCode }}</h1>
+      <p class="statusMessage">{{ error.message }}</p>
+      <InputButton
+        :text="$t('go_back_btn_error_page')"
+        @click="handleError"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+const error = useError()
+const l = useLocalePath()
+
+const handleError = () => {
+  clearError({
+    redirect: l('/'),
+  })
+}
+</script>
+
+<style scoped>
+.container {
+  display: grid;
+  place-items: center;
+  width: 100%;
+  height: 100vh;
+  margin-top: auto;
+  margin-bottom: auto;
+}
+
+.container div {
+  display: grid;
+  place-items: center;
+}
+
+.statusMessage {
+  color: #ededee;
+  font-size: 20px;
+}
+
+.statusCode {
+  color: #ededee;
+  font-size: 50px;
+}
+</style>
