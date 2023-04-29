3364f3554dce28fb9d6a43370c7147959a7771a6
René Scheuer <rene.scheuer03@gmail.com>
2023-04-29 16:28:17 +0200

    AC-25

---
+++ components/AddEntry.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="grid grid-cols-2 gap-x-4 cursor-pointer -ml-28"
+    @click="emits('onClick')"
+  >
+    <div class="place-self-end">
+      <button>
+        <nuxt-img
+          :width="width"
+          :height="height"
+          :src="imageUrl"
+          :alt="`Button for ${name}`"
+        />
+      </button>
+    </div>
+    <div class="">
+      <h2 class="text-x1 text-red-400 font-semibold">{{ name }}</h2>
+      <p class="text-primary text-xs">{{ label }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  imageUrl: String,
+  name: String,
+  label: String,
+  width: Number,
+  height: Number,
+})
+
+const emits = defineEmits(['onClick'])
+</script>
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,23 +1,61 @@
 <template>
-  <div class="grid grid-cols-3 responsive-1 absolute bottom-3 left-0 md:left-auto place-items-center px-6">
+  <div
+    v-if="isActive"
+    class="grid grid-cols-3 responsive-1 absolute bottom-9 md:left-auto px-6 space-y-4"
+  >
+    <h1 class="row-start-1 col-start-2 self-end place-self-center text-primary text-x1 font-semibold">{{ $t('add') }}</h1>
     <BtnWithImg
-      width="45"
-      height="45"
+      :width="20"
+      :height="20"
+      class="row-start-1 col-start-3 place-self-end"
+      :name="$t('close_add_habit_or_entry')"
+      image-url="/icons/hub/goal_icon.png"
+      @on-click="isActive = !isActive"
+    />
+    <div class="row-start-2 col-span-3">
+      <AddEntry
+        class=""
+        :width="50"
+        :height="50"
+        :name="$t('habit')"
+        image-url="/icons/hub/goal_icon.png"
+        :label="$t('add_habit')"
+        @on-click="isActive = !isActive"
+      />
+      <AddEntry
+        class=""
+        :width="50"
+        :height="50"
+        :name="$t('diary')"
+        image-url="/icons/hub/diary_icon.png"
+        :label="$t('add_diary_entry')"
+        @on-click="isActive = !isActive"
+      />
+    </div>
+  </div>
+  <div
+    v-if="!isActive"
+    class="grid grid-cols-3 responsive-1 absolute bottom-3 left-0 md:left-auto place-items-center px-6"
+  >
+    <BtnWithImg
+      :width="45"
+      :height="45"
       :name="$t('habits')"
       image-url="/icons/hub/habits_icon.png"
       :label="$t('habits')"
       @on-click="navigateTo(l('/habits'))"
     />
     <BtnWithImg
-      width="55"
-      height="55"
+      :width="55"
+      :height="55"
       class="-mt-14"
       :name="$t('add_new_habit_or_diary_entry')"
       image-url="/icons/hub/add_icon.png"
+      @on-click="isActive = !isActive"
     />
     <BtnWithImg
-      width="45"
-      height="45"
+      :width="45"
+      :height="45"
       :name="$t('diary')"
       image-url="/icons/hub/diary_icon.png"
       :label="$t('diary')"
@@ -28,4 +66,5 @@
 
 <script setup>
 const l = useLocalePath()
+const isActive = ref(false)
 </script>
