f9252c1c480834a7994085d2f2df26fb6d1454d9
Soheil Nazari <kontakt@soheilnazari.de>
2023-04-30 17:50:47 +0200

    cleanup

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,40 +1,38 @@
 <template>
   <div
     v-if="isActive"
-    class="grid grid-cols-3 responsive-1 absolute bottom-9 md:left-auto px-6 space-y-4"
+    class="grid place-items-center gap-8 pb-8 pt-4 px-6 responsive-1 absolute bottom-0 md:left-auto bg-[#1b2a4a] rounded-t-3xl"
   >
-    <h1 class="row-start-1 col-start-2 self-end place-self-center text-primary text-x1 font-semibold">{{ $t('add') }}</h1>
-    <BtnWithImg
-      :width="20"
-      :height="20"
-      class="row-start-1 col-start-3 place-self-end"
-      :name="$t('close_add_habit_or_entry')"
+    <div class="flex items-center">
+      <h1 class="text-center text-primary text-xl">{{ $t('add') }}</h1>
+      <button
+        class="text-primary font-semibold text-2xl absolute right-6"
+        @click="isActive = !isActive"
+      >
+        X
+      </button>
+    </div>
+    <AddEntry
+      class=""
+      :width="50"
+      :height="50"
+      :name="$t('habit')"
       image-url="/icons/hub/goal_icon.png"
+      :label="$t('add_habit')"
       @on-click="isActive = !isActive"
     />
-    <div class="row-start-2 col-span-3">
-      <AddEntry
-        class=""
-        :width="50"
-        :height="50"
-        :name="$t('habit')"
-        image-url="/icons/hub/goal_icon.png"
-        :label="$t('add_habit')"
-        @on-click="isActive = !isActive"
-      />
-      <AddEntry
-        class=""
-        :width="50"
-        :height="50"
-        :name="$t('diary_entry')"
-        image-url="/icons/hub/diary_icon.png"
-        :label="$t('add_diary_entry')"
-        @on-click="isActive = !isActive"
-      />
-    </div>
+    <AddEntry
+      class=""
+      :width="50"
+      :height="50"
+      :name="$t('diary_entry')"
+      image-url="/icons/hub/diary_icon.png"
+      :label="$t('add_diary_entry')"
+      @on-click="isActive = !isActive"
+    />
   </div>
   <div
-    v-if="!isActive"
+    v-else
     class="grid grid-cols-3 responsive-1 absolute bottom-3 left-0 md:left-auto place-items-center px-6"
   >
     <BtnWithImg
