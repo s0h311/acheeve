f85ee62791446719db3edd975545754e16a94afc
Soheil Nazari <kontakt@soheilnazari.de>
2023-05-03 10:42:16 +0200

    cleanup

--- components/Hub.vue
+++ components/Hub.vue
@@ -10,8 +10,8 @@
           alt=""
         />
       </button>
-      <div class="grid place-items-center gap-8 mt-8">
-        <h1 class="text-primary font-semibold text-[26px] leading-8 whitespace-pre-wrap text-center">{{ getWelcomeText() }}</h1>
+      <div class="grid place-items-center gap-8">
+        <h1 class="text-primary font-semibold text-[26px] leading-8 whitespace-pre-wrap text-center">{{ welcomeText }}</h1>
         <HubSliderCal
           days-in-each-direc="14"
           @date-change="onDateChange"
@@ -27,10 +27,6 @@
 </template>
 
 <script setup lang="ts">
-definePageMeta({
-  layout: 'centered',
-})
-
 const { t } = useI18n()
 const l = useLocalePath()
 
@@ -53,7 +49,7 @@ const months = [
   t('december'),
 ]
 
-const getWelcomeText = (): string => {
+const welcomeText = computed<string>((): string => {
   let day = selectedDate.value.getDate()
   let month = months[selectedDate.value.getMonth()]
 
@@ -68,10 +64,16 @@ const getWelcomeText = (): string => {
     : t('date_th')
 
   return firstWord + ' ' + day + numberExtension + ' ' + month + '\n' + habitsLeft.value + ' ' + t('habits_left_welcome_text_hub')
-}
+})
 
 const onDateChange = (date: Date) => {
   selectedDate.value = date
   // update to do and habit cards
 }
 </script>
+
+<style scoped>
+.responsive-w {
+  @apply w-full sm:w-4/6 md:w-3/6 lg:w-2/6 xl:w-[20%] 2xl:w-[27%];
+}
+</style>
