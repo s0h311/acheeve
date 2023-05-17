e8f17925d83f403ad27f05957367d278e969c5cc
René Scheuer <rene.scheuer03@gmail.com>
2023-05-17 14:26:57 +0200

    shortened AC-57

--- composables/useWindowSize.ts
+++ composables/useWindowSize.ts
@@ -2,20 +2,11 @@ import { useWindowSize } from '@vueuse/core'
 
 const { width, height } = useWindowSize()
 
-export default function useScreenSize() {
-  const isPhone = ref(false)
-  const isTablet = ref(false)
-  const isLaptop = ref(false)
-  const isBigDisplay = ref(false)
-
-  const checkScreenSize = () => {
-    isPhone.value = width.value < 640
-    isTablet.value = width.value >= 640 && width.value < 1024
-    isLaptop.value = width.value >= 1024 && width.value < 1920
-    isBigDisplay.value = width.value >= 1920
-  }
-
-  checkScreenSize()
+export default () => {
+  let isPhone = width.value < 640
+  let isTablet = width.value >= 640 && width.value < 1024
+  let isLaptop = width.value >= 1024 && width.value < 1920
+  let isBigDisplay = width.value >= 1920
 
   return {
     isPhone,
